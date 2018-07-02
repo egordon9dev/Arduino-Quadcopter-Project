c8b34c61447248fc68ace921c2e8c316d00287ad
egordon9dev <egordon9dev@gmail.com>
2018-07-01 23:43:17 -0500

    PID tuned. Stable version.

--- arduino_quad_V4/arduino_quad_V4.ino
+++ arduino_quad_V4/arduino_quad_V4.ino
@@ -1,14 +1,12 @@
 #include <SPI.h>
+#include <Servo.h>
 #include "I2Cdev.h"
 #include "MPU6050_6Axis_MotionApps20.h"
 #include "RF24.h"
+#include "Wire.h"
 #include "nRF24L01.h"
 #include "pid.h"
 #include "printf.h"
-#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
-#include "Wire.h"
-#endif
-#include <Servo.h>
 
 // AD0 low = 0x68
 // AD0 high = 0x69
@@ -18,224 +16,233 @@ MPU6050 mpu;
 Servo escfl, escfr, escbl, escbr;
 #define LONG_MAX 2147483647
 //---- roll ----
-PidVars roll_pid = {.unwind = 1.0, .doneZone = 0, .maxInteg = 400, .iActiveZone = 3, .target = 0, .sensVal = 0, .prevSensVal = 0, .integ = 0, .kp = 0, .ki = 0, .kd = 0, .deriv = 0, .prevTime = 0, .doneTime = 0, .prevDUpdateTime = 0};
+PidVars roll_pid = {.unwind = 0.0, .doneZone = 0, .maxInteg = 150, .iActiveZone = 15 /*5 TURN THIS ON AGAIN*/, .target = 0, .sensVal = 0, .prevSensVal = 0, .integ = 0, .kp = 0, .ki = 0, .kd = 0, .deriv = 0, .minSignificantOutput = 25, .prevTime = 0, .doneTime = 0};
 //---- pitch ----
-PidVars pitch_pid = {.unwind = 1.0, .doneZone = 0, .maxInteg = 400, .iActiveZone = 3, .target = 0, .sensVal = 0, .prevSensVal = 0, .integ = 0, .kp = 0, .ki = 0, .kd = 0, .deriv = 0, .prevTime = 0, .doneTime = 0, .prevDUpdateTime = 0};
+PidVars pitch_pid = {.unwind = 0.0, .doneZone = 0, .maxInteg = 150, .iActiveZone = 15 /*5*/, .target = 0, .sensVal = 0, .prevSensVal = 0, .integ = 0, .kp = 0, .ki = 0, .kd = 0, .deriv = 0, .minSignificantOutput = 25, .prevTime = 0, .doneTime = 0};
 //---- yaw ----
-PidVars yaw_pid = {.unwind = 1.0, .doneZone = 0, .maxInteg = 400, .iActiveZone = 3, .target = 0, .sensVal = 0, .prevSensVal = 0, .integ = 0, .kp = 0, .ki = 0, .kd = 0, .deriv = 0, .prevTime = 0, .doneTime = 0, .prevDUpdateTime = 0};
-int radioLED = 4, pgmLED = 10;
-unsigned long prevRadioUpdateT = 0;
+PidVars yaw_pid = {.unwind = 0.0, .doneZone = 0, .maxInteg = 50, .iActiveZone = 15 /*5*/, .target = 0, .sensVal = 0, .prevSensVal = 0, .integ = 0, .kp = 0, .ki = 0, .kd = 0, .deriv = 0, .minSignificantOutput = 9, .prevTime = 0, .doneTime = 0};
+int radioLED = 4, pgmLED = 8;
 
-bool dmpReady = false;   // set true if DMP init was successful
-uint8_t mpuIntStatus;    // holds actual interrupt status byte from MPU
-uint8_t devStatus;       // return status after each device operation (0 = success, !0
-                         // = error)
-uint16_t packetSize;     // expected DMP packet size (default is 42 bytes)
-uint16_t fifoCount;      // count of all bytes currently in FIFO
+bool dmpReady = false;  // set true if DMP init was successful
+uint8_t mpuIntStatus;  // holds actual interrupt status byte from MPU
+uint8_t devStatus;  // return status after each device operation (0 = success, !0
+                    // = error)
+uint16_t packetSize;  // expected DMP packet size (default is 42 bytes)
+uint16_t fifoCount;  // count of all bytes currently in FIFO
 uint8_t fifoBuffer[64];  // FIFO storage buffer
 
-Quaternion q;         // [w, x, y, z]         quaternion container
+Quaternion q;  // [w, x, y, z]         quaternion container
 VectorFloat gravity;  // [x, y, z]            gravity vector
-float euler[3];       // [psi, theta, phi]    Euler angle container
-float ypr[3];         // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
+float euler[3];  // [psi, theta, phi]    Euler angle container
+float ypr[3];  // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
 
-RF24 radio(7, 8);
+RF24 radio(7, 10);
+#define PAYLOAD_SIZE 16
+#define RADIO_SIGNATURE 197
 
 volatile bool mpuInterrupt = false;  // indicates whether MPU interrupt pin has gone high
 // ISR
 void dmpDataReady() { mpuInterrupt = true; }
 
-const int MIN_MOTOR_POWER = 0;
-const int MAX_MOTOR_POWER = 739;
+const int MIN_MOTOR_POWER = 1000;
+const int MAX_MOTOR_POWER = 2000;  // 1650
+const int dataLength = PAYLOAD_SIZE / 2;
+uint16_t data[dataLength];
 void setup() {
-    Serial.begin(115200);
-    printf_begin();
-    escfl.attach(9);
-    escfr.attach(6);
-    escbr.attach(5);
-    escbl.attach(3);
-    escfl.writeMicroseconds(1000);
-    escfr.writeMicroseconds(1000);
-    escbl.writeMicroseconds(1000);
-    escbr.writeMicroseconds(1000);
+  Serial.begin(115200);
+  printf_begin();
+  Serial.print("init.");
+  escfl.attach(9, MIN_MOTOR_POWER, MAX_MOTOR_POWER);
+  escfr.attach(6, MIN_MOTOR_POWER, MAX_MOTOR_POWER);
+  escbr.attach(5, MIN_MOTOR_POWER, MAX_MOTOR_POWER);
+  escbl.attach(3, MIN_MOTOR_POWER, MAX_MOTOR_POWER);
+  escfl.writeMicroseconds(MIN_MOTOR_POWER);
+  escfr.writeMicroseconds(MIN_MOTOR_POWER);
+  escbl.writeMicroseconds(MIN_MOTOR_POWER);
+  escbr.writeMicroseconds(MIN_MOTOR_POWER);
 
-// join I2C bus (I2Cdev library doesn't do this automatically)
-#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
-    Wire.begin();
-    TWBR = 24;  // 400kHz I2C clock (200kHz if CPU is 8MHz)
-#elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
-    Fastwire::setup(400, true);
-#endif
+  Serial.print(".");
+  Wire.begin();
+  TWBR = 12;  // 24;
 
-    mpu.initialize();
+  mpu.initialize();
 
-    devStatus = mpu.dmpInitialize();
-    //-2935  -1145 1339  122 69  10
-    mpu.setXAccelOffset(-2935);
-    mpu.setYAccelOffset(-1145);
-    mpu.setZAccelOffset(1339);  // 1688 factory default for my test chip
-    mpu.setXGyroOffset(122);
-    mpu.setYGyroOffset(69);
-    mpu.setZGyroOffset(10);
+  devStatus = mpu.dmpInitialize();
+  mpu.setXAccelOffset(-2932);
+  mpu.setYAccelOffset(-1125);
+  mpu.setZAccelOffset(1325);
+  mpu.setXGyroOffset(117);
+  mpu.setYGyroOffset(72);
+  mpu.setZGyroOffset(-9);
 
-    // make sure it worked (returns 0 if so)
-    if (devStatus == 0) {
-        mpu.setDMPEnabled(true);
-        attachInterrupt(0, dmpDataReady, RISING);
-        mpuIntStatus = mpu.getIntStatus();
-        dmpReady = true;
-        packetSize = mpu.dmpGetFIFOPacketSize();
-    }
+  // make sure it worked (returns 0 if so)
+  if (devStatus == 0) {
+    mpu.setDMPEnabled(true);
+    attachInterrupt(0, dmpDataReady, RISING);
+    mpuIntStatus = mpu.getIntStatus();
+    dmpReady = true;
+    packetSize = mpu.dmpGetFIFOPacketSize();
+  }
+  Serial.print(".");
 
-    // configure LED for output
-    pinMode(radioLED, OUTPUT);
-    pinMode(pgmLED, OUTPUT);
-    digitalWrite(radioLED, 0);
-    digitalWrite(pgmLED, 0);
-    radio.begin();
-    radio.setAutoAck(0);
-    radio.setRetries(15, 15);
-    radio.setPayloadSize(10);
-    radio.openReadingPipe(1, (byte *)("ctrlr"));
-    radio.startListening();
-    radio.setPALevel(RF24_PA_MAX);
-    radio.setDataRate(RF24_250KBPS);
-    radio.setCRCLength(RF24_CRC_8);
-    radio.setChannel(20);
-
-    for (int i = 30; i > 0; i--) {
-        Serial.println(i);
-        delay(1000);
-    }
+  // configure LED for output
+  pinMode(radioLED, OUTPUT);
+  pinMode(pgmLED, OUTPUT);
+  digitalWrite(radioLED, 0);
+  digitalWrite(pgmLED, 0);
+  radio.begin();
+  Serial.print(".");
+  radio.setAutoAck(0);
+  radio.setRetries(15, 15);
+  radio.setPayloadSize(PAYLOAD_SIZE);
+  radio.openReadingPipe(1, (byte *)("ctrlr"));
+  radio.startListening();
+  radio.setPALevel(RF24_PA_MAX);
+  radio.setDataRate(RF24_250KBPS);
+  radio.setCRCLength(RF24_CRC_8);
+  radio.setChannel(76);
+  Serial.println(" done");
+  for (int i = 0; i < dataLength; i++) { data[i] = 0; }
+  for (int i = 30; i > 0; i--) {
+    Serial.println(i);
+    delay(700);
+  }
 }
-
-uint16_t data[5] = {3, 3, 3, 3, 3};
-double deltaTime = 0.0;
-long time0 = millis() - 5;
-int throttle = MIN_MOTOR_POWER;
+int limInt(int n, int min, int max) {
+  if (n < min) return min;
+  if (n > max) return max;
+  return n;
+}
+double limDbl(double n, double min, double max) {
+  if (n < min) return min;
+  if (n > max) return max;
+  return n;
+}
+int throttle = 0;
 // double kpYaw = 0.7, kiYaw = 0.28, kdYaw = 0.6, targetYaw = 0.0, errorYaw =
 // 0.0, totalErrorYaw = 0.0, prevErrorYaw = 0.0; double kpPitch = 3.4, kiPitch =
 // 0.28, kdPitch = 2.0, targetPitch = 0.0, errorPitch = 0.0, totalErrorPitch =
 // 0.0, prevErrorPitch = 0.0; double kpRoll = 3.4, kiRoll = 0.28, kdRoll = 2.0,
 // targetRoll = 0.0, errorRoll = 0.0, totalErrorRoll = 0.0, prevErrorRoll = 0.0;
 bool bCal = false;
-int outFL = MIN_MOTOR_POWER, outFR = MIN_MOTOR_POWER, outBL = MIN_MOTOR_POWER, outBR = MIN_MOTOR_POWER;
+int outFL = 0, outFR = 0, outBL = 0, outBR = 0;
 double pitchCal = 0, rollCal = 0, yawCal = 0;
 int calCtr = 0;
-unsigned long lastUpdateT = 0, lastRadioT = 0;
+unsigned long lastRadioT = 0;
+bool fail = false;
+unsigned long numLoops = 0;
+double pitch = 0, roll = 0, yaw = 0;
+int8_t yawSc = 0;
+unsigned long yawUpdateT = 0;
+bool yawPidRunning = true;
 void loop() {
-    deltaTime = (double(millis() - time0)) / 1000.0;
-    time0 = millis();
+  if (!dmpReady) return;
 
-    if (!dmpReady) return;
-
-    // wait for MPU interrupt or extra packet(s) available
-    while (!mpuInterrupt && fifoCount < packetSize) {}
-
+  radio.startListening();
+  byte n;
+  if (radio.available(&n)) {
+    if (n == 1) {
+      uint16_t temp[dataLength];
+      radio.read(&temp, PAYLOAD_SIZE);
+      if ((uint8_t)(temp[7] >> 8) == RADIO_SIGNATURE) {
+        for (int i = 0; i < dataLength; i++) { data[i] = temp[i]; }
+        lastRadioT = millis();
+      } else {
+        lastRadioT = 0;
+      }
+    }
+  }
+  // wait for MPU interrupt or extra packet(s) available
+  if (mpuInterrupt || fifoCount >= packetSize) {
     // reset interrupt flag and get INT_STATUS byte
     mpuInterrupt = false;
     mpuIntStatus = mpu.getIntStatus();
 
     // get current FIFO count
     fifoCount = mpu.getFIFOCount();
-
-    // check for overflow (this should never happen unless our code is too
-    // inefficient)
     if ((mpuIntStatus & 0x10) || fifoCount == 1024) {
-        mpu.resetFIFO();
-        // check for interupt
+      mpu.resetFIFO();
     } else if (mpuIntStatus & 0x02) {
-        while (fifoCount < packetSize) fifoCount = mpu.getFIFOCount();
-        mpu.getFIFOBytes(fifoBuffer, packetSize);
-
-        // track FIFO count here in case there is > 1 packet available
-        // (this lets us immediately read more without waiting for an interrupt)
-        fifoCount -= packetSize;
-        radio.startListening();
-        // if there is data ready
-        byte n;
-        if (radio.available(&n)) {
-            prevRadioUpdateT = millis();
-            radio.read(&data, sizeof(uint16_t[5])); /*
-Serial.print("recieved:\t");
-Serial.print("pipe ");
-Serial.print(n);
-if(n == 1) Serial.print("yay");
-Serial.print("\t");
-for(int i = 0; i < 4; i++) {
-  Serial.print((int)data[i]);
-  Serial.print("\t");
-}
-Serial.print(data[4] >> 8);
-Serial.print("\t");
-Serial.print(data[4] & 0b11111111);
-Serial.print("\t");
-Serial.println();*/
-            pitch_pid.kp = data[0] / 50.0;
-            pitch_pid.ki = 0;  // data[1] / 100000.0;
-            pitch_pid.kd = data[2] / 3.0;
-            roll_pid.kp = pitch_pid.kp;
-            roll_pid.ki = pitch_pid.ki;
-            roll_pid.kd = pitch_pid.kd;
-            throttle = data[3] / 2;
-            pitch_pid.target = 0;  //((int)(data[4] >> 8) - 127) / 10.0;
-            roll_pid.target = 0;   //((int)(data[4] & 0b11111111) - 127) / 10.0;
-            yaw_pid.target = 0;
-            lastRadioT = millis();
+      while (fifoCount < packetSize) fifoCount = mpu.getFIFOCount();
+      mpu.getFIFOBytes(fifoBuffer, packetSize);
+      mpu.dmpGetQuaternion(&q, fifoBuffer);
+      mpu.dmpGetGravity(&gravity, &q);
+      mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
+      fifoCount -= packetSize;
+      yaw_pid.kp = 1.0 * (roll_pid.kp = pitch_pid.kp = /*data[0]*/ 425.0 / 150.0);
+      yaw_pid.ki = 1.0 * (roll_pid.ki = pitch_pid.ki = /*data[1]*/ 30.0 / 40000.0);
+      yaw_pid.kd = 1.0 * (roll_pid.kd = pitch_pid.kd = /*data[2]*/ 400.0);
+      throttle = data[3] * (975.0 / 1023.0) - 100;
+      if (!bCal) {
+        pitchCal += ypr[1];
+        rollCal += ypr[2];
+        yawCal += ypr[0];
+        calCtr++;
+        if (calCtr == 500) {
+          bCal = true;
+          pitchCal /= (double)calCtr;
+          rollCal /= (double)calCtr;
+          yawCal /= (double)calCtr;
         }
-        // display Euler angles in degrees
-        mpu.dmpGetQuaternion(&q, fifoBuffer);
-        mpu.dmpGetGravity(&gravity, &q);
-        mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
-        int pitch = 0, roll = 0, yaw = 0;
-        if (!bCal) {
-            pitchCal += ypr[1];
-            rollCal += ypr[2];
-            yawCal += ypr[0];
-            calCtr++;
-            if (calCtr == 500) {
-                bCal = true;
-                pitchCal /= (double)calCtr;
-                rollCal /= (double)calCtr;
-                yawCal /= (double)calCtr;
-            }
-        } else {
-            roll_pid.sensVal = (ypr[1] - pitchCal) * 180.0 / PI;
-            pitch_pid.sensVal = (ypr[2] - rollCal) * 180.0 / PI;
-            yaw_pid.sensVal = (ypr[0] - yawCal) * 180.0 / PI;
-            pitch = updatePID(&pitch_pid);
-            roll = updatePID(&roll_pid);
-            yaw = 0;  // updatePID(&yaw_pid);
+      } else {
+        roll_pid.sensVal = (ypr[1] - pitchCal) * 180.0 / PI;
+        pitch_pid.sensVal = (ypr[2] - rollCal) * 180.0 / PI;
+        if (fabs(roll_pid.sensVal) > 60 || fabs(pitch_pid.sensVal) > 60) fail = true;
+        yaw_pid.sensVal = (ypr[0] - yawCal) * 180.0 / PI;
+        pitch_pid.target = ((int)(data[4] >> 8) - 127) / 8.0;
+        roll_pid.target = ((int)(data[4] & 0b11111111) - 127) / 8.0;
+        yawSc = (int8_t)(data[7] & 0xFF) / 3;
+        if (yawSc != 0) {
+          yawUpdateT = millis();
+          yawPidRunning = false;
         }
-        if (millis() - lastRadioT < 250) {
-            outFL = throttle + roll - pitch;
-            outFR = throttle - roll - pitch;
-            outBL = throttle + roll + pitch;
-            outBR = throttle - roll + pitch;
-            if (outFL < MIN_MOTOR_POWER) outFL = MIN_MOTOR_POWER;
-            if (outFL > MAX_MOTOR_POWER) outFL = MAX_MOTOR_POWER;
-            if (outFR < MIN_MOTOR_POWER) outFR = MIN_MOTOR_POWER;
-            if (outFR > MAX_MOTOR_POWER) outFR = MAX_MOTOR_POWER;
-            if (outBL < MIN_MOTOR_POWER) outBL = MIN_MOTOR_POWER;
-            if (outBL > MAX_MOTOR_POWER) outBL = MAX_MOTOR_POWER;
-            if (outBR < MIN_MOTOR_POWER) outBR = MIN_MOTOR_POWER;
-            if (outBR > MAX_MOTOR_POWER) outBR = MAX_MOTOR_POWER;
+        if (millis() - yawUpdateT > 50 && !yawPidRunning) {
+          yawPidRunning = true;
+          yaw_pid.target = yaw_pid.sensVal;
         }
-        printf("F %4d %4d B %4d %4d   pry %+05d/%+05d %+05d/%+05d %+05d/%+05d\n", 1258 + outFL, 1025 + outFR, 1261 + outBL, 1026 + outBR, (int)(100 * pitch_pid.sensVal), (int)(100 * pitch_pid.target), (int)(100 * roll_pid.sensVal), (int)(100 * roll_pid.target), (int)(100 * yaw_pid.sensVal), (int)(100 * yaw_pid.target));
+        if (millis() - lastRadioT <= 250 && !fail) {
+          pitch = updatePID(&pitch_pid);
+          roll = updatePID(&roll_pid);
+          yaw = yawPidRunning ? limDbl(updatePID(&yaw_pid), -100, 100) : -yawSc;
+          outFL = throttle + roll - pitch - yaw - ((int16_t)data[6]) * 0.5 + 0.5;
+          outFR = throttle - roll - pitch + yaw - ((int16_t)data[6]) * 0.5 - ((int16_t)data[5]) * 0.5 + 0.5;
+          outBL = throttle + roll + pitch + yaw + 0.5;
+          outBR = throttle - roll + pitch - yaw - ((int16_t)data[5]) * 0.5 + 0.5;
+          escfl.writeMicroseconds(limInt(MIN_MOTOR_POWER + outFL, MIN_MOTOR_POWER, MAX_MOTOR_POWER));
+          escfr.writeMicroseconds(limInt(MIN_MOTOR_POWER + outFR, MIN_MOTOR_POWER, MAX_MOTOR_POWER));
+          escbl.writeMicroseconds(limInt(MIN_MOTOR_POWER + outBL, MIN_MOTOR_POWER, MAX_MOTOR_POWER));
+          escbr.writeMicroseconds(limInt(MIN_MOTOR_POWER + outBR, MIN_MOTOR_POWER, MAX_MOTOR_POWER));
+          /*Serial.print(escfl.readMicroseconds());
+          Serial.print("  ");
+          Serial.print(escfr.readMicroseconds());
+          Serial.print("  ");
+          Serial.print(escbr.readMicroseconds());
+          Serial.print("  ");
+          Serial.println(escbl.readMicroseconds());*/
+        }
+      } /*
+          printf("pry %+05d/%+05d %+05d/%+05d %+05d/%+05d\n", (int)(100 * pitch_pid.sensVal), (int)(100 * pitch_pid.target), (int)(100 * roll_pid.sensVal), (int)(100 * roll_pid.target), (int)(100 * yaw_pid.sensVal), (int)(100 * yaw_pid.target));*/
     }
+  }
 
-    if (millis() - lastRadioT > 250 || !bCal) {
-        escfl.writeMicroseconds(1000);
-        escfr.writeMicroseconds(1000);
-        escbl.writeMicroseconds(1000);
-        escbr.writeMicroseconds(1000);
-    } else if (millis() - lastUpdateT > 2) {
-        escfl.writeMicroseconds(1258 + outFL);
-        escfr.writeMicroseconds(1098 /*1098*/ + outFR);
-        escbl.writeMicroseconds(1188 /*1188*/ + outBL);
-        escbr.writeMicroseconds(1026 + outBR);
-        lastUpdateT = millis();
-    }
-    if (millis() - prevRadioUpdateT < 250) { digitalWrite(radioLED, (millis() / 50) % 10 == 0); }
-    digitalWrite(pgmLED, (millis() / 30) % 4 == 0);
+  if (millis() - lastRadioT > 250 || !bCal || fail) {
+    escfl.writeMicroseconds(MIN_MOTOR_POWER);
+    escfr.writeMicroseconds(MIN_MOTOR_POWER);
+    escbl.writeMicroseconds(MIN_MOTOR_POWER);
+    escbr.writeMicroseconds(MIN_MOTOR_POWER);
+  }
+  if (millis() - lastRadioT < 250) { digitalWrite(radioLED, (millis() / 50) % 10 == 0); }
+  digitalWrite(pgmLED, (millis() / 30) % 4 == 0);
 }
+
+/*
+TODO----------------------
+
+
+
+
+
+
+
+
+*/
--- joystick_and_radios_controller/joystick_and_radios_controller.ino
+++ joystick_and_radios_controller/joystick_and_radios_controller.ino
@@ -1,61 +1,168 @@
-#include <SPI.h>
-#include "nRF24L01.h"
-#include "RF24.h"
-#include "printf.h"
-#include <SoftwareSerial.h>
 #include <LiquidCrystal_I2C.h>
+#include <SPI.h>
 #include "Nunchuk.h"
+#include "RF24.h"
+#include "nRF24L01.h"
+#include "printf.h"
+
+#define TRIM(i) (digitalRead(trim[i]))
+#define PAYLOAD_SIZE 16
+#define RADIO_SIGNATURE 197
 
 LiquidCrystal_I2C lcd(0x3f, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);
-RF24 radio(7, 8);
-
-void setup(void)
-{
-  Serial.begin(9600);
+RF24 radio(7, 10);
+int8_t trim[] = {4, 5, 6, 9};  // left, top, right, bottom
+volatile bool prevTrim[] = {false, false, false, false};
+volatile int16_t trimCtr[] = {-155, -46};
+volatile bool radioOn = true, prevD8 = true;
+volatile unsigned long debounceT[] = {0, 0, 0, 0, 0};
+inline void enablePCI() { PCICR |= 0b00000101; }
+inline void clearPCI() { PCICR &= 0b11111010; }
+int limInt(int n, int min, int max) {
+  if (n < min) return min;
+  if (n > max) return max;
+  return n;
+}
+uint16_t data[PAYLOAD_SIZE / 2];
+void setup(void) {
+  Serial.begin(115200);
   printf_begin();
   Wire.begin();
   nunchuk_init();
-  lcd.begin(16,2);
+  for (int i = 0; i < 4; i++) { pinMode(trim[i], INPUT); }
+  pinMode(8, INPUT_PULLUP);
+  lcd.begin(16, 2);
   lcd.backlight();
   lcd.clear();
-  lcd.setCursor(0,0);
+  lcd.setCursor(0, 0);
   lcd.print("init");
   radio.begin();
   radio.setAutoAck(0);
   radio.setRetries(15, 15);
-  radio.setPayloadSize(10);
+  radio.setPayloadSize(PAYLOAD_SIZE);
   radio.openWritingPipe((byte*)("ctrlr"));
   radio.setPALevel(RF24_PA_MAX);
   radio.setDataRate(RF24_250KBPS);
   radio.setCRCLength(RF24_CRC_8);
-  radio.setChannel(20);
+  radio.setChannel(76);
+  for (int i = 0; i < PAYLOAD_SIZE / 2; i++) { data[i] = 0; }
+  //---- use interupts for trim buttons ----
+  PCIFR |= 0b00000101;  // clear interupt flags
+  PCMSK2 |= 0b01110000;  // enable pin change interupts on pins D4, D5, and D6
+  PCMSK0 |= 0b00000011;  // enable pin change interupts on pin D8, D9
+  enablePCI();  // enable pin change interupts
+  printf("init\n");
 }
-int8_t rollUnsc = 0, pitchUnsc = 0;
+#define DEBOUNCE_INTERVAL 80
+ISR(PCINT0_vect) {  // D8 to D13
+  if ((PINB & 0b00000010) && !prevTrim[3] && millis() - debounceT[3] > DEBOUNCE_INTERVAL) {  // D9
+    trimCtr[1]--;
+    prevTrim[3] = true;
+    debounceT[3] = millis();
+  } else if (!(PINB & 0b00000001) && prevD8 && millis() - debounceT[4] > 1000) {  // D8
+    // radioOn = !radioOn;
+    prevD8 = false;
+    debounceT[4] = millis();
+  }
+}
+ISR(PCINT2_vect) {  // D0 to D7
+  if ((PIND & 0b00010000) && !prevTrim[0] && millis() - debounceT[0] > DEBOUNCE_INTERVAL) {  // D4
+    trimCtr[0]--;
+    prevTrim[0] = true;
+    debounceT[0] = millis();
+  } else if ((PIND & 0b00100000) && !prevTrim[1] && millis() - debounceT[1] > DEBOUNCE_INTERVAL) {  // D5
+    trimCtr[1]++;
+    prevTrim[1] = true;
+    debounceT[1] = millis();
+  } else if ((PIND & 0b01000000) && !prevTrim[2] && millis() - debounceT[2] > DEBOUNCE_INTERVAL) {  // D6
+    trimCtr[0]++;
+    prevTrim[2] = true;
+    debounceT[2] = millis();
+  }
+}
+int8_t rollUnsc = 0, pitchUnsc = 0, yawUnsc = 0;
+long lastRadioT = 0, lastLCDT = 0;
+uint8_t oldSREG;
 void loop(void) {
-  if(nunchuk_read()) {
-    rollUnsc = nunchuk_joystickX();
-    pitchUnsc = nunchuk_joystickY();
+  /*while (true) {
+      printf("%d %d %d %d\n", digitalRead(trimL), digitalRead(trimT), digitalRead(trimR), digitalRead(trimB));
+      delay(100);
+  }*/
+  if (millis() - lastRadioT > 10) {
+    lastRadioT = millis();
+    //----- atomic block ------
+    clearPCI();
+    if (nunchuk_read()) {
+      rollUnsc = limInt(nunchuk_joystickX() + 1, -120, 120);
+      pitchUnsc = limInt(nunchuk_joystickY(), -120, 120);
+    }
+    enablePCI();
+    //-------------------------
+    //----- atomic block ------
+    oldSREG = SREG;
+    cli();
+    int kpUnsc = analogRead(A0), kiUnsc = analogRead(A1), kdUnsc = analogRead(A2), thrUnsc = analogRead(A3);
+    yawUnsc = limInt(analogRead(A2) / 5 - 103, -127, 127);
+    SREG = oldSREG;
+    //-------------------------
+    data[0] = kpUnsc;
+    data[1] = kiUnsc;
+    data[2] = kdUnsc;
+    data[3] = thrUnsc;
+    data[4] = ((pitchUnsc + 127) << 8) + rollUnsc + 127;
+    data[5] = trimCtr[0];
+    data[6] = trimCtr[1];
+    data[7] = (RADIO_SIGNATURE << 8) + (uint8_t)yawUnsc;
+    if (radioOn) {
+      //----- atomic block ------
+      clearPCI();
+      radio.stopListening();
+      radio.write(&data, PAYLOAD_SIZE);
+      enablePCI();
+      //-------------------------
+    }
   }
-  int kpUnsc = analogRead(A0), kiUnsc = analogRead(A1),
-  kdUnsc = analogRead(A2), thrUnsc = analogRead(A3);
-    
-  uint16_t data[5] = {kpUnsc, kiUnsc, kdUnsc, thrUnsc, ((pitchUnsc + 127) << 8) + rollUnsc + 127};
-  radio.stopListening();
-  radio.write(&data, sizeof(uint16_t[5]));
-  Serial.print("sent:\t");
-  for(int i = 0; i < 4; i++) {
-    Serial.print(data[i]);
-    Serial.print("\t");
+  /*
+   Serial.print("sent:\t");
+   for (int i = 0; i < 4; i++) {
+       Serial.print(data[i]);
+       Serial.print("\t");
+   }
+   Serial.print(data[4] >> 8);
+   Serial.print("\t");
+   Serial.print(data[4] & 0b11111111);
+   Serial.print("\t");
+   Serial.println();*/
+  if (millis() - lastLCDT > 100) {
+    lastLCDT = millis();
+    char s[15] = "";
+    //----- atomic block -----
+    oldSREG = SREG;
+    cli();
+    uint16_t temp[] = {data[0], data[1], data[2], data[3], data[4], data[5], data[6]};
+    SREG = oldSREG;
+    //------------------------
+    sprintf(s, "%4d %4d %4d", temp[0], temp[1], temp[2]);
+    //----- atomic block -----
+    clearPCI();
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print(s);
+    enablePCI();
+    //-------------------------
+    sprintf(s, "%4d %+4d %+4d %s", temp[3], (int16_t)trimCtr[0], (int16_t)trimCtr[1], radioOn ? "1" : "0");
+    //----- atomic block -----
+    clearPCI();
+    lcd.setCursor(0, 1);
+    lcd.print(s);
+    enablePCI();
+    //-------------------------
+    printf("END LCD\n");
   }
-  Serial.print(data[4] >> 8);
-  Serial.print("\t");
-  Serial.print(data[4] & 0b11111111);
-  Serial.print("\t");
-  Serial.println();
-  char s[15] = "";
-  sprintf(s, "%4d %4d %4d", data[0], data[1], data[2]);
-  lcd.clear();
-  lcd.setCursor(0,0);
-  lcd.print(s);
-  delay(50);
+  prevD8 = digitalRead(8);
+  for (int i = 0; i < 4; i++) { prevTrim[i] = TRIM(i); }
+  delayMicroseconds(500);
 }
+/*
+200 106 293
+*/
